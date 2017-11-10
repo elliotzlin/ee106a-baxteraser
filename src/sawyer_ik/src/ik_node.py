fa06d848d36f12f70e52d6f952aa480f465d99eb
Elliot Z. Lin <elliot.z.lin@berkeley.edu>
2017-11-10 01:21:18 -0800

    Finally get some working protoype of a node in; now have a Subscriber node that is also controllable!

--- src/sawyer_ik/src/ik_node.py
+++ src/sawyer_ik/src/ik_node.py
@@ -3,13 +3,26 @@ import rospy
 from moveit_msgs.srv import GetPositionIK, GetPositionIKRequest, GetPositionIKResponse
 from geometry_msgs.msg import PoseStamped
 from moveit_commander import MoveGroupCommander
-from ar_track_alvar.msg import AlvarMarkers
+from ar_track_alvar_msgs.msg import AlvarMarkers
+
+msg = ''
 
 # Listener to handle pulling information about the AR tag(s)
 def ar_tag_listener():
-    rospy.init_node('ar_tag_subscriber', anonymous=True)
-    rospy.Subscriber('/ar_pose_marker', AlvarMarkers, ar_tag_callback)
-    rospy.spin()
+    rospy.Subscriber('/ar_pose_marker', AlvarMarkers, transform_ar_tag)
+
+def transform_ar_tag(message):
+    global msg
+    msg = message
+
+def inverse_kinematics(message): 
+    print(message)
+    # Construct the request
+    request = GetPositionIKRequest()
+    request.ik_request.group_name = "right_arm"
+    request.ik_request.ik_link_name = "right_gripper"
+    request.ik_request.attempts = 50
+    request.ik_request.pose_stamped.header.frame_id = "base"
 
 if __name__ == '__main__':
     # Wait for the IK service to become available
@@ -19,8 +32,8 @@ if __name__ == '__main__':
     # Create the function used to call the service
     compute_ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
 
+    # Create a subscriber to get AR tag positions
+    ar_tag_listener()
     while not rospy.is_shutdown():
-        raw_input('Position the AR tag and hit <Enter> to compute an IK solution: ')
-
-        # Construct the request
-        request = GetPositionIKRequest()
+        raw_input('Hit Enter')
+        inverse_kinematics(msg)
