a4f2f88d8022bda5ff6f3fb76d5ed41f7ded1ce9
Elliot Z. Lin <elliot.z.lin@berkeley.edu>
2017-11-09 16:50:33 -0800

    New package, here we gogit status :package:

---
+++ src/sawyer_ik/src/ik_node.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import rospy
+from moveit_msgs.srv import GetPositionIK, GetPositionIKRequest, GetPositionIKResponse
+from geometry_msgs.msg import PoseStamped
+from moveit_commander import MoveGroupCommander
+
+if __name__ == '__main__':
+    # Wait for the IK service to become available
+    rospy.wait_for_service('compute_ik')
+    rospy.init_node('service_query')
+
+    # Create the function 
