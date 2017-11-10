9c516a88fdaf00dbdd1ec28e251708fd4bd2ddb2
Elliot Z. Lin <elliot.z.lin@berkeley.edu>
2017-11-09 17:02:05 -0800

    More scripting, but gotta go to class now :rage1:

--- src/sawyer_ik/src/ik_node.py
+++ src/sawyer_ik/src/ik_node.py
@@ -3,10 +3,24 @@ import rospy
 from moveit_msgs.srv import GetPositionIK, GetPositionIKRequest, GetPositionIKResponse
 from geometry_msgs.msg import PoseStamped
 from moveit_commander import MoveGroupCommander
+from ar_track_alvar.msg import AlvarMarkers
+
+# Listener to handle pulling information about the AR tag(s)
+def ar_tag_listener():
+    rospy.init_node('ar_tag_subscriber', anonymous=True)
+    rospy.Subscriber('/ar_pose_marker', AlvarMarkers, ar_tag_callback)
+    rospy.spin()
 
 if __name__ == '__main__':
     # Wait for the IK service to become available
     rospy.wait_for_service('compute_ik')
     rospy.init_node('service_query')
 
-    # Create the function 
+    # Create the function used to call the service
+    compute_ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
+
+    while not rospy.is_shutdown():
+        raw_input('Position the AR tag and hit <Enter> to compute an IK solution: ')
+
+        # Construct the request
+        request = GetPositionIKRequest()
