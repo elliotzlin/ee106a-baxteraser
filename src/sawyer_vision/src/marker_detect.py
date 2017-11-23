801f848ad21801f645476ab44d5c7a3051dce701
Elliot Z. Lin <elliot.z.lin@berkeley.edu>
2017-11-22 16:59:18 -0800

    Homography returns matrix; untested as of yet

--- src/sawyer_vision/src/marker_detect.py
+++ src/sawyer_vision/src/marker_detect.py
@@ -23,6 +23,8 @@ class image_converter:
         self.fast.setBool('nonmaxSuppression',0)
         self.fast.setInt('threshold', 30)
 
+        self.H = 0
+
     def callback(self, data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -31,19 +33,62 @@ class image_converter:
         (rows,cols,channels) = cv_image.shape
         crop = cv_image[150:rows-150,150:cols-150]
         kp = self.fast.detect(crop, None)
-        print(len(kp))
         for k in kp:
             x = k.pt[0] + 150
             y = k.pt[1] + 150
             k.pt = (x,y)
         img2 = cv2.drawKeypoints(cv_image, kp, color=(255,0,0))
-        cv2.imshow("Image window", img2)
+        squares = find_squares(cv_image)
+        sqr = 0
+        # Get valid square coordinate
+        for sq in squares:
+            min_x = 1000
+            for point in sq:
+                if point[0] < min_x:
+                    min_x = point[0]
+            if min_x < 500:
+                continue
+            else:
+                sqr = sq
+                break
+        # Calculate homography
+        spatial_points = np.array([[TAG_SIZE,TAG_SIZE], [0,TAG_SIZE], [0,0], [TAG_SIZE,0]])
+        retval, mask = cv2.findHomography(spatial_points, sqr.astype(float)) # Cast as float else will throw error
+        # Retval returned above is our homography transformation matrix
+        self.H = retval
+        cv2.drawContours(cv_image, [sqr], 0, (0, 255, 0), 3)
+        cv2.imshow("Image window", cv_image)
         cv2.waitKey(3)
 
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
         except CvBridgeError as e:
             print(e)
+
+# Find squares function taken from the opencv sample
+def find_squares(img):
+    def angle_cos(p0, p1, p2):
+        d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
+        return abs(np.dot(d1, d2) / np.sqrt(np.dot(d1,d1)*np.dot(d2,d2)))
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    squares = []
+    for gray in cv2.split(img):
+        for thrs in range(0, 255, 26):
+            if thrs == 0:
+                bin = cv2.Canny(gray, 0, 50, apertureSize=5)
+                bin = cv2.dilate(bin, None)
+            else:
+                _retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
+            contours, _hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            for cnt in contours:
+                cnt_len = cv2.arcLength(cnt, True)
+                cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
+                if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
+                    cnt = cnt.reshape(-1, 2)
+                    max_cos = np.max([angle_cos(cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4]) for i in range(4)])
+                    if max_cos < 0.1:
+                        squares.append(cnt)
+    return squares
 
 def main(args):
     ic = image_converter()
